6ccd340ba9d18b912f191e9a7a1d7b74da63da30
rimpapakira <rimpapakira037@gmail.com>
2025-11-19 12:05:37 +0530

    add slider

--- js/index.js
+++ js/index.js
@@ -1,5 +1,52 @@
+// Scroll To Top
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
-
- AOS.init();
+// AOS
+AOS.init();
+//  Slider
+$(document).ready(function () {
+  $(".testimonial-slider").slick({
+    centerMode: true,
+    centerPadding: "0",
+    slidesToShow: 3,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
